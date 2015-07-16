8357564c1f10ce2de5e678255a8dd4f922846933
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-07-16 08:29:43 +0800

    FEATURE : Extracted redis package into new class.

--- hitchredis/__init__.py
+++ hitchredis/__init__.py
@@ -1,1 +1,2 @@
 from hitchredis.redis_service import RedisService
+from hitchredis.redis_package import RedisPackage
--- hitchredis/redis_service.py
+++ hitchredis/redis_service.py
@@ -3,21 +3,13 @@ import subprocess
 import os
 
 class RedisService(Service):
-    def __init__(self, version, redis_exec="redis-server", redis_cli="redis-cli", port=16379, **kwargs):
-        self.version = version
+    def __init__(self, redis_package, port=16379, **kwargs):
+        self.redis_package = redis_package
         self.port = port
-        self.redis_exec = redis_exec
-        self.redis_cli = redis_cli
-        kwargs['command'] = ["redis-server", "--port", str(port)]
+        kwargs['command'] = [self.redis_package.server, "--port", str(port)]
         kwargs['log_line_ready_checker'] = lambda line: "The server is now ready to accept connections" in line
         super(RedisService, self).__init__(**kwargs)
 
-    def setup(self):
-        self.log("Checking redis version...")
-        version_output = self.subcommand(self.redis_exec, "--version").run(check_output=True)
-        if self.version not in version_output:
-            raise RuntimeError("Redis version needed is {}, output is {}.".format(self.version, version_output))
-
     def cli(self, *args, **kwargs):
-        cmd = [self.redis_cli, "-p", str(self.port)] + list(args)
+        cmd = [self.redis_package.cli, "-p", str(self.port)] + list(args)
         return self.subcommand(*cmd, **kwargs)
